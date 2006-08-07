695a68841256e5f23ef51e708d6cd00e3b3347a7
Matthew Allum <mallum@openedhand.com>
2006-08-07 20:53:51 +0000

    add ogles hack

---
+++ tests/test.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/keysym.h>
+
+#include "GLES/gl.h"
+#include "GLES/egl.h"
+
